c9f9176f46b0ba31750f86531dd1b86bd7d737e6
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2022-02-18 10:01:23 -0800

    Create onLoad.R

---
+++ R/onLoad.R
@@ -0,0 +1,3 @@
+.onLoad <- function(libname, pkgname) {
+  cat(paste0("Welcome to the '", pkgname, "' package. Please see https://github.com/nikola-sur/goodGLM for information on how to cite this package."))
+}
